4c4493d7f4f3c73087369133784e0ed2519db820
emdyomo <ladesulaiman33@gmail.com>
2025-03-14 03:57:36 -0700

    Update page

--- src/app/Stats/page.tsx
+++ src/app/Stats/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Image from "next/image";
 import { motion } from "framer-motion";
-import { FaBars, FaTimes, FaUsers, FaBriefcase, FaRegCalendarCheck, FaTwitter, FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
+
 
 export default function Stats() {
-    const [menuOpen, setMenuOpen] = useState(false);
     function AnimatedCounter({ value }: { value: number }) {
         const [count, setCount] = useState(0);
     

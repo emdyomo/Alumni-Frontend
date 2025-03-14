a193567c7cdeba9d393cfc491f2114f972b0fc96
emdyomo <ladesulaiman33@gmail.com>
2025-03-14 03:53:32 -0700

    Update Navbar

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Image from "next/image";
 import { motion } from "framer-motion";
-import { FaBars, FaTimes, FaUsers, FaBriefcase, FaRegCalendarCheck, FaTwitter, FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 export default function Navbar() {
     const [menuOpen, setMenuOpen] = useState(false);
